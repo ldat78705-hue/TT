5fa3dac68b8116e4edc955a7e067638e25db2c24
ldat78705-hue <ldat78705-hue@users.noreply.github.com>
2026-06-14 13:26:37 +0700

    chore: bump app to v2.0 (versionCode 11) - add zaloUserId field

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.educenter.pro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.9"
+        versionCode = 11
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
