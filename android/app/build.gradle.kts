c6432f0a3872537a2d2c093a32e8e4fbbc78c024
ldat78705-hue <ldat78705-hue@users.noreply.github.com>
2026-06-14 18:32:36 +0700

    chore: bump app version to v2.1 (versionCode 12)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.educenter.pro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "2.0"
+        versionCode = 12
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
