164a30aa35bb70f17486eb73c6e54bf61034ecf2
ldat78705-hue <ldat78705-hue@users.noreply.github.com>
2026-06-15 00:34:13 +0700

    chore: Bump version to 2.4, upgrade Gradle 8.11.1 + AGP 8.7.3 for JDK 25 compat

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.educenter.pro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "2.3"
+        versionCode = 15
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
