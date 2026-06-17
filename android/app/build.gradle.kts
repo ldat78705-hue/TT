5ae736e44022768864f12c6cd65fd0611fe71bea
ldat78705-hue <ldat78705-hue@users.noreply.github.com>
2026-06-17 21:32:15 +0700

    release: bump version to v2.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.educenter.pro"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "2.7.1"
+        versionCode = 20
+        versionName = "2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
